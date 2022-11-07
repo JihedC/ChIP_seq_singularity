9d93e7f4d6389d30e76f47eb107928d53afc2741
JihedC <jihedc@pm.me>
2022-11-07 10:25:20 +0100

    added --use-singularity into snakemake call

--- slurm_snakemake.sh
+++ slurm_snakemake.sh
@@ -6,15 +6,24 @@
 #SBATCH -t 24:00:00
 #SBATCH --mem=15000
 
+######################## Loading modules required by the workflow ########################
 module purge
 module load genomics/ngs/samtools/1.11/gcc-8.3.1
 module load genomics/ngs/aligners/bowtie2/2.4.2/gcc-8.3.1
+module load container/singularity/3.10.0/gcc.8.5.0
+# If the workflow does not work anymore, 
+# check that the modules are still correctly installed on the HPC
+# they may have been removed or updated
+
+#########################################################################################
+
 echo Start time : `date`
 snakemake -p \
         --snakefile Snakefile \
         --latency-wait 60 \
         --wait-for-files \
         --rerun-incomplete \
+        --use-singularity \
         --use-conda \
         --cluster "sbatch --parsable --partition=all --mem=36g --ntasks=1 --cpus-per-task=8 --time=24:00:00 --hint=multithread" \
  	--cluster-status "./slurm-cluster-status.py" \
